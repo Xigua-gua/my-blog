4cdbc585d2608cbcda2a415c85e8f1091b6b2d03
Xigua-gua <Xigua-gua@users.noreply.github.com>
2018-02-24 11:32:58 +0800

    Update blogMain.js

--- static/js/blogMain.js
+++ static/js/blogMain.js
@@ -1,15 +1,6 @@
 
 //这是前端的 AJAX 请求方法
 var ajax = function(request) {
-    /*
-    request 是一个 object, 有如下属性
-        method, 请求的方法, string
-        url, 请求的路径, string
-        data, 请求发送的数据, 如果是 GET 方法则没这个值, string
-        callback, 响应回调, function
-
-    本题不会就放弃, 本题带了一个用法在下方
-    */
     var r = new XMLHttpRequest()
     r.open(request.method, request.url, true)
     if (request.contentType !== undefined) {
